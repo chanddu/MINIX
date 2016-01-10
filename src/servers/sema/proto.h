a38f5700d1e49654390512a74256d53d3444ebdd
chanddu <chandug96@gmail.com>
2016-01-10 19:25:15 +0530

    added proto.h

---
+++ src/servers/sema/proto.h
@@ -0,0 +1,9 @@
+/* Function prototypes. */
+
+
+
+/* _seminit.c _semdown.c _semup.c _semrelease.c */
+_PROTOTYPE( int sem_init, (int start_value)					);
+_PROTOTYPE( int sem_down, (int sem_number)					);
+_PROTOTYPE( int sem_up, (int sem_number)		);
+_PROTOTYPE( int sem_release, (int sem)			);
